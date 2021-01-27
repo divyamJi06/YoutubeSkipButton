ac2ef7288b1b3c051cc98239779ec6d8edec93de
Divyam <divyamgupta2001@gmail.com>
2021-01-27 21:20:21 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,18 @@
+
+timeout = setInterval(() => {
+
+    skipButton = document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0]
+    if (skipButton != undefined) {
+        console.log(skipButton)
+        if (skipButton.innerText == "Skip Ad" || skipButton.innerText == "Skip Ads") {
+            skipButton.click()
+            console.log("Button Clicked")
+            clearInterval(timeout)
+        }
+        
+        if (skipButton.innerText == "") {
+            console.log("Closed")
+            clearInterval(timeout)
+        }
+    }
+}, 1000);
